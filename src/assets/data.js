115113ee76731c06c784a66268ec55dbf0bd040b
Vishal-1514 <vishalverma1514@gmail.com>
2025-09-27 09:20:27 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        question: "What does the word enthusiast mean?",
+        option1: "Someone who is passionate about something",
+        option2: "Someone who is lazy",
+        option3: "Someone who dislikes an activity",
+        option4: "SSomeone who is confused",
+        ans: 1,
+    },
+    {
+        question: "What does the word enthusiast mean?",
+        option1: "Someone who is passionate about something",
+        option2: "Someone who is lazy",
+        option3: "Someone who dislikes an activity",
+        option4: "SSomeone who is confused",
+        ans: 1,
+
+    },
+    {
+        question: "What does the word enthusiast mean?",
+        option1: "Someone who is passionate about something",
+        option2: "Someone who is lazy",
+        option3: "Someone who dislikes an activity",
+        option4: "SSomeone who is confused",
+        ans: 1,
+    },
+    {
+        question: "What does the word enthusiast mean?",
+        option1: "Someone who is passionate about something",
+        option2: "Someone who is lazy",
+        option3: "Someone who dislikes an activity",
+        option4: "SSomeone who is confused",
+        ans: 1,
+    },
+    {
+        question: "What does the word enthusiast mean?",
+        option1: "Someone who is passionate about something",
+        option2: "Someone who is lazy",
+        option3: "Someone who dislikes an activity",
+        option4: "SSomeone who is confused",
+        ans: 1, 
+    }
+
+]
