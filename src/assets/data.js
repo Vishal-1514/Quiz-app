5d4dfee53ffd58cf18df47e7c31a2c16e153b2cd
Vishal-1514 <vishalverma1514@gmail.com>
2025-09-27 16:12:55 +0530

    quiz update

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,11 +1,12 @@
 export const data = [
     {
-        question: "What does the word enthusiast mean?",
-        option1: "Someone who is passionate about something",
-        option2: "Someone who is lazy",
-        option3: "Someone who dislikes an activity",
-        option4: "SSomeone who is confused",
-        ans: 1,
+        question: "Who won the Ballon d’Or in 2023?",
+        option1: "Erling Haaland",
+        option2: "Lionel Messi ",
+        option3: "Kylian Mbappé",
+        option4: "Karim Benzema",
+        ans: 2,
+         
     },
     {
         question: "What does the word enthusiast mean?",
@@ -17,28 +18,33 @@ export const data = [
 
     },
     {
-        question: "What does the word enthusiast mean?",
-        option1: "Someone who is passionate about something",
-        option2: "Someone who is lazy",
-        option3: "Someone who dislikes an activity",
-        option4: "SSomeone who is confused",
-        ans: 1,
+        question: "Which country has won the most FIFA World Cups?",
+        option1: "Italy",
+        option2: "Germany",
+        option3: "Brazil",
+        option4: "Argentina",
+        ans: 2,
+        
+
+ 
     },
     {
-        question: "What does the word enthusiast mean?",
-        option1: "Someone who is passionate about something",
-        option2: "Someone who is lazy",
-        option3: "Someone who dislikes an activity",
-        option4: "SSomeone who is confused",
+        question: "In which year was the first FIFA World Cup held?",
+        option1: "B) 1930 ",
+        option2: "1934",
+        option3: "1940",
+        option4: "1950",
         ans: 1,
+         
     },
     {
-        question: "What does the word enthusiast mean?",
-        option1: "Someone who is passionate about something",
-        option2: "Someone who is lazy",
-        option3: "Someone who dislikes an activity",
-        option4: "SSomeone who is confused",
-        ans: 1, 
+        question: "Which club is nicknamed “The Red Devils”?",
+        option1: "Arsenal",
+        option2: "Liverpool",
+        option3: "Manchester United",
+        option4: "AC Milan",
+        ans: 3, 
+        
     }
 
 ]
